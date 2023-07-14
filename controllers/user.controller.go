dec4a95388221b689ae005be98bb6016640ff7d8
Akbar Maliki <akbarmaliki109@gmail.com>
2023-07-14 20:59:53 +0700

    add routes group for user

--- controllers/user.controller.go
+++ controllers/user.controller.go
@@ -26,15 +26,23 @@ func (uc *UserController) GetUser(ctx *gin.Context) {
 
 func (uc *UserController) GetAll(ctx *gin.Context) {
 	ctx.JSON(200, "")
-
 }
 
 func (uc *UserController) UpdateUser(ctx *gin.Context) {
 	ctx.JSON(200, "")
-
 }
 
 func (uc *UserController) DeleteUser(ctx *gin.Context) {
 	ctx.JSON(200, "")
+}
 
+// grouping the route for anything that has prefix /user routes
+func (uc *UserController) RegisterUserRoutes(registerGroup *gin.RouterGroup) {
+	userRoute := registerGroup.Group("/user")
+
+	userRoute.POST("/create", uc.CreateUser)
+	userRoute.GET("/get/:name", uc.GetUser)
+	userRoute.GET("/get", uc.GetAll)
+	userRoute.POST("/update", uc.UpdateUser)
+	userRoute.DELETE("/delete/:name", uc.DeleteUser)
 }
