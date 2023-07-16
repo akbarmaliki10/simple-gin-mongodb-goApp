37669ee90bcc3c10c262abebaf7e73c5a1ffd1b8
Akbar Maliki <akbarmaliki109@gmail.com>
2023-07-16 17:24:21 +0700

    add service and controller implementation

--- controllers/user.controller.go
+++ controllers/user.controller.go
@@ -1,7 +1,9 @@
 package controllers
 
 import (
+	"golangGin/models"
 	"golangGin/service"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
@@ -17,23 +19,60 @@ func NewUserController(userService service.UserService) UserController {
 }
 
 func (uc *UserController) CreateUser(ctx *gin.Context) {
-	ctx.JSON(200, "")
+	var user models.User
+	if err := ctx.ShouldBindJSON(&user); err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"message": err.Error()})
+		return
+	}
+	err := uc.UserService.CreateUser(&user)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"message": err.Error()})
+	}
+	ctx.JSON(http.StatusOK, gin.H{"message": "success"})
 }
 
 func (uc *UserController) GetUser(ctx *gin.Context) {
-	ctx.JSON(200, "")
+	var username string = ctx.Param("name")
+	user, err := uc.UserService.GetUser(&username)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"message": err.Error()})
+		return
+	}
+
+	ctx.JSON(http.StatusOK, user)
 }
 
 func (uc *UserController) GetAll(ctx *gin.Context) {
-	ctx.JSON(200, "")
+	users, err := uc.UserService.GetAll()
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"message": err.Error()})
+		return
+	}
+	ctx.JSON(http.StatusOK, users)
 }
 
 func (uc *UserController) UpdateUser(ctx *gin.Context) {
-	ctx.JSON(200, "")
+	var user models.User
+	if err := ctx.ShouldBindJSON(&user); err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"message": err.Error()})
+		return
+	}
+	err := uc.UserService.UpdateUser(&user)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"message": err.Error()})
+		return
+	}
+	ctx.JSON(http.StatusOK, gin.H{"message": "success"})
 }
 
 func (uc *UserController) DeleteUser(ctx *gin.Context) {
-	ctx.JSON(200, "")
+	username := ctx.Param("name")
+	err := uc.UserService.DeleteUser(&username)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"message": err.Error()})
+		return
+	}
+	ctx.JSON(http.StatusOK, gin.H{"message": "success"})
 }
 
 // grouping the route for anything that has prefix /user routes
--- service/user.service.impl.go
+++ service/user.service.impl.go
@@ -2,8 +2,10 @@ package service
 
 import (
 	"context"
+	"errors"
 	"golangGin/models"
 
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
@@ -22,21 +24,57 @@ func NewUserService(userCollection *mongo.Collection, ctx context.Context) UserS
 
 // receiver function or more like classes/struct method in python/java
 func (u *UserServiceImpl) CreateUser(user *models.User) error {
-	return nil
+	_, err := u.userCollection.InsertOne(u.ctx, user)
+	return err
 }
 
 func (u *UserServiceImpl) GetUser(name *string) (*models.User, error) {
-	return nil, nil
+	var user *models.User
+	query := bson.D{bson.E{Key: "name", Value: name}}
+	err := u.userCollection.FindOne(u.ctx, query).Decode(&user)
+	return user, err
 }
 
 func (u *UserServiceImpl) GetAll() ([]*models.User, error) {
-	return nil, nil
+	var users []*models.User
+	cursor, err := u.userCollection.Find(u.ctx, bson.D{{}})
+	if err != nil {
+		return nil, err
+	}
+	for cursor.Next(u.ctx) {
+		var user *models.User
+		err := cursor.Decode(&user)
+		if err != nil {
+			return nil, err
+		}
+		users = append(users, user)
+	}
+	if err := cursor.Err(); err != nil {
+		return nil, err
+	}
+	cursor.Close(u.ctx)
+
+	if len(users) == 0 {
+		return nil, errors.New("document is empty")
+	}
+	return users, nil
 }
 
-func (u *UserServiceImpl) UpdateUser(models *models.User) error {
+func (u *UserServiceImpl) UpdateUser(user *models.User) error {
+	filter := bson.D{bson.E{Key: "user_name", Value: user.Name}}
+	update := bson.D{bson.E{Key: "$set", Value: bson.D{bson.E{Key: "user_name", Value: user.Name}, bson.E{Key: "user_age", Value: user.Age}, bson.E{Key: "user_address", Value: user.Address}}}}
+	result, _ := u.userCollection.UpdateOne(u.ctx, filter, update)
+	if result.MatchedCount != 1 {
+		return errors.New("no matched document found for update")
+	}
 	return nil
 }
 
 func (u *UserServiceImpl) DeleteUser(name *string) error {
+	filter := bson.D{bson.E{Key: "user_name", Value: name}}
+	result, _ := u.userCollection.DeleteOne(u.ctx, filter)
+	if result.DeletedCount == 0 {
+		return errors.New("no matched document found for delete")
+	}
 	return nil
 }
