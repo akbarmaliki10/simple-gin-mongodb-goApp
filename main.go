94389070ad02dd762e2e1c399c75d1da70eb595d
Akbar Maliki <akbarmaliki109@gmail.com>
2023-07-16 21:51:07 +0700

    complete main function and add db connection

--- main.go
+++ main.go
@@ -1,11 +1,53 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"golangGin/controllers"
+	"golangGin/service"
+	"log"
 
-func main() {
-	fmt.Println(saySomething())
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
+)
+
+var (
+	server         *gin.Engine
+	userService    service.UserService
+	userController controllers.UserController
+	ctx            context.Context
+	userCollection *mongo.Collection
+	mongoClient    *mongo.Client
+	err            error
+)
+
+func init() {
+	ctx = context.TODO()
+	mongoConn := options.Client().ApplyURI("mongodb://localhost:27017")
+	mongoClient, err := mongo.Connect(ctx, mongoConn)
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = mongoClient.Ping(ctx, readpref.Primary())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("MongoDB connection is successfull!")
+
+	userCollection = mongoClient.Database("userdb").Collection("users")
+	userService = service.NewUserService(userCollection, ctx)
+	userController = controllers.NewUserController(userService)
+	server = gin.Default()
 }
 
-func saySomething() (string, string) {
-	return "something ", "else"
+func main() {
+	defer mongoClient.Disconnect(ctx)
+
+	basePath := server.Group("/v1")
+	userController.RegisterUserRoutes(basePath)
+
+	log.Fatal(server.Run(":8080"))
 }
